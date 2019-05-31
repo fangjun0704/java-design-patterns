a898ea8a4c6c9b80d8c84ebd7d8bf93f5aecaf5f
yangjiegeng <yangjiegeng@wacai.com>
2019-05-31 17:26:54 +0800

    feat: 观察者模式

--- src/main/java/com/company/project/behavior/BehaviorMain.java
+++ src/main/java/com/company/project/behavior/BehaviorMain.java
@@ -9,11 +9,11 @@ import com.company.project.interfaces.impl.PorkFood;
 public class BehaviorMain {
 
   public static void main(String[] args) {
-    //策略模式
+    //【策略模式】
     StrategyFood strategyFood = new StrategyFood(new PorkFood());
     strategyFood.nameOrType();
 
-    //观察者模式
+    //【观察者模式】
     //先定义一个主题
     SubjectMy subject = new SubjectMy();
     //定义两个观察者
@@ -22,5 +22,7 @@ public class BehaviorMain {
 
     //更新状态(会立即通知所有的观察者们)
     subject.setState(5);
+
+
   }
 }
